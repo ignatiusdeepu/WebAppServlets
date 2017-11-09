e36d57c96d94bb4fab292e8389eec931fcc6d395
Ignatius Joseph <ignatiusdeepu@gmail.com>
2017-11-09 15:31:54 -0500

    no message

---
+++ StudentPortalWeb/src/org/students/dao/CourseDao.java
@@ -0,0 +1,16 @@
+package org.students.dao;
+
+import java.util.List;
+
+import org.students.bean.CourseBean;
+
+public interface CourseDao extends EntityDao{
+
+	public static final StringBuilder INSERT_INTO_COURSE = new StringBuilder("INSERT INTO COURSE(CODE,NAME,DESCRIPTION,CREATE_DT) VALUES(?,?,?,?)");
+	
+	public static final StringBuilder SELECT_ALL_COURSES = new StringBuilder("SELECT ID,CODE,NAME,CREATE_DT,DESCRIPTION FROM COURSE");
+	
+	public boolean insertCourse(CourseBean course);
+	
+	public List<CourseBean> findAllCourses();
+}
---
+++ StudentPortalWeb/src/org/students/service/CourseService.java
@@ -0,0 +1,12 @@
+package org.students.service;
+
+import java.util.List;
+
+import org.students.bean.CourseBean;
+
+public interface CourseService {
+
+	boolean addCourse(CourseBean course) throws Exception;
+	
+	public List<CourseBean> findAllCourses();
+}
